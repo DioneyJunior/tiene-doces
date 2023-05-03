5b15d2087d6f8088c697dddc9e409855842d31e5
Dioney Junior <dioneyfbjunior@gmail.com>
2023-05-03 08:54:12 -0300

    Footer changes

--- src/components/FooterSite.vue
+++ src/components/FooterSite.vue
@@ -1,57 +1,34 @@
 <template>
-  <!-- Footer -->
-  <footer class="bg-dark text-center text-white mt-auto">
-    <!-- Grid container -->
-    <div class="container p-4 pb-0">
-      <!-- Section: Social media -->
-      <section class="mb-4">
-        <!-- Facebook -->
-        <a
-          class="btn text-light btn-floating m-1"
-          href="https://www.facebook.com/marciran.brisida"
-          target="_blank"
-          role="button"
-        >
-          <i class="bi bi-facebook facebook-button"></i
-        ></a>
-
-        <!-- Instagram -->
-        <a
-          class="btn text-light btn-floating m-1"
-          href="https://www.instagram.com/psi.marciranbrisida"
-          target="_blank"
-          role="button"
-        >
-          <i class="bi bi-instagram instagram-button"></i>
-        </a>
-
-        <!-- WhatsApp -->
-        <a
-          class="btn text-light btn-floating m-1"
-          href="https://wa.me/+5535988434758"
-          target="_blank"
-          role="button"
-          ><i class="bi bi-whatsapp whatsapp-button"></i
-        ></a>
-
-        <!-- Email (precisa colocar link) -->
-        <a
-          class="btn text-light btn-floating m-1"
-          href="mailto:marciranbrisidapsi@gmail.com"
-          target="_blank"
-          role="button"
-          ><i class="bi bi-envelope-fill email-button"></i
-        ></a>
-      </section>
-      <!-- Section: Social media -->
+  <footer>
+    <div class="footer-content">
+      <h3>Tiene Doces Finos</h3>
+      <p>Doces para seu evento</p>
     </div>
-    <!-- Grid container -->
-
-    <!-- Copyright -->
-    <div class="text-center p-3 credits" style="background-color: rgba(0, 0, 0, 0.2)">
-      © 2023 Marciran Brisida - Todos os direitos reservados | Desenvolvido por Kelvin Flora
+    <div>
+      <ul class="socials">
+        <li>
+          <a href="https://www.instagram.com/tiene_doces_finos/" target="_blank"
+            ><i class="bi bi-instagram"></i
+          ></a>
+        </li>
+        <li>
+          <a href="https://wa.me/+554497602781" target="_blank"> <i class="bi bi-whatsapp"></i></a>
+        </li>
+      </ul>
     </div>
-    <!-- Copyright -->
+    <div class="footer-menu">
+      <ul class="f-menu">
+        <li><router-link class="nav-link" to="/">Início</router-link></li>
+        <li><router-link class="nav-link" to="/servicos">Serviços</router-link></li>
+        <li><router-link class="nav-link" to="/contato">Contato</router-link></li>
+      </ul>
+    </div>
+    <div class="footer-bottom">
+      <p>
+        copyright &copy;2023
+        <a href="https://github.com/DioneyJunior" target="_blank">Dioney Junior</a>
+      </p>
+    </div>
   </footer>
 </template>
 <script>
@@ -60,19 +37,91 @@ export default {
 };
 </script>
 <style>
-.facebook-button:hover {
-  color: rgb(252, 212, 112);
+footer {
+  background-color: #4c0c0c;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: auto;
+  padding-top: 40px;
+  color: #ce9e60;
+}
+.footer-content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+}
+.footer-content h3 {
+  color: #ce9e60;
+  font-size: 2.1rem;
+  font-weight: 500;
+  text-transform: capitalize;
+}
+.socials {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-left: 0;
+}
+.socials li {
+  margin: 0 10px;
 }
 
-.instagram-button:hover {
-  color: rgb(252, 212, 112);
+.socials a {
+  text-decoration: none;
+  color: #ce9e60;
+  padding: 5px;
+}
+.socials a i {
+  font-size: 1.1rem;
+  width: 20px;
+  transition: color 0.4s ease;
+}
+.socials a:hover i {
+  color: #e99f3e;
+}
+.footer-bottom {
+  background: #3b0a0a;
+  padding: 10px;
+  text-align: center;
+}
+.footer-bottom p {
+  font-size: 14px;
+  word-spacing: 2px;
+  text-transform: capitalize;
+}
+.footer-bottom p a {
+  color: #e99f3e;
+  font-size: 16px;
+  text-decoration: none;
+}
+.footer-bottom span {
+  text-transform: uppercase;
+  opacity: 0.4;
+  font-weight: 200;
 }
 
-.whatsapp-button:hover {
-  color: rgb(252, 212, 112);
+.footer-menu ul {
+  display: flex;
+  padding-left: 0;
+  margin-bottom: 0;
+  justify-content: center;
 }
 
-.email-button:hover {
-  color: rgb(252, 212, 112);
+.footer-menu ul li {
+  padding-right: 10px;
+  display: block;
+}
+
+.footer-menu ul li a {
+  color: #ce9e60;
+  text-decoration: none;
+}
+
+.footer-menu ul li a:hover {
+  color: #e99f3e;
 }
 </style>
